54fdbfb99207cefa1f80d363d359e11efa518709
alpha-154 <mrhrakib07@gmail.com>
2024-12-25 02:09:38 +0600

    fixed add service problem

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import AllServices from "@/pages/AllServices";
-import AddService from "@/pages/Addservice";
+import AddService from "@/pages/AddService";
 import BookedServices from "@/pages/BookedServices";
 import ManagedServices from "@/pages/ManagedServices";
 import ServiceDetails from "@/pages/ServiceDetails";
